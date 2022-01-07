532bbe5cf26d79123c5893e7a14f53463cc2a660
oyhub <73133152+oyhub@users.noreply.github.com>
2022-01-07 22:44:32 +0100

    Add to cart
    Add to cart indicator

--- js/product.js
+++ js/product.js
@@ -2,6 +2,7 @@
 const plussBtn = document.querySelector("#btn-plus");
 const minusBtn = document.querySelector("#btn-minus");
 const quantity = document.querySelector("#btn-quantity");
+const addToCart = document.querySelector("#add-to-cart");
 
 // Change quantity
 let quantityValue = parseInt(quantity.value);
@@ -23,3 +24,12 @@ function valueDown() {
 
 plussBtn.addEventListener("click", valueUp);
 minusBtn.addEventListener("click", valueDown);
+
+//Shop indicator
+
+function greenBox() {
+  addToCart.innerHTML = "Add to cart";
+  addToCart.innerHTML += `<p id="added-to-cart">Added to cart</p>`;
+}
+
+addToCart.addEventListener("click", greenBox);
